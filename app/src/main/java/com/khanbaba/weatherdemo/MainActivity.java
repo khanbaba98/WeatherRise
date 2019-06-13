d5e8b3a10a16a1b93f4df0844d7736f7b56abdd2
Rahil Khan <khan98rahil@gmail.com>
2019-06-13 20:28:27 +0530

    first commit

---
+++ app/src/main/java/com/khanbaba/weatherdemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.khanbaba.weatherdemo;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //splashScreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dashBoardIntent = new Intent(MainActivity.this, DashBoardActivity.class);
+                startActivity(dashBoardIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
